2650a2638acc20e8dc1ca9ae6f77b9ab07d7531d
mukhtorbabadjanov <mukhtorbabadjanov@gmail.com>
2022-02-16 15:37:42 +0500

    asd

--- frontend/views/layouts/_menu.php
+++ frontend/views/layouts/_menu.php
@@ -1072,7 +1072,7 @@ $name = "name_" . Yii::$app->language;
                                         <?= Html::a(Yii::t('app', 'Ta\'lim resurslari') . " <i class='fa fa-angle-down'></i>", '#', ['class' => 'menu1']) ?>
                                         <ul class="submenu">
 
-                                            <li><?= Html::a(Yii::t('app', 'Ilmiy ishlar'), 'https://urdu.uz/site/active?id=181') ?></li>
+                                            <li><?= Html::a(Yii::t('app', 'Elektron adabiyot'), 'https://urdu.uz/site/active?id=181') ?></li>
                                             <li><?= Html::a(Yii::t('app', 'Call markaz'), 'https://urdu.uz/site/active?id=177') ?></li>
                                             <li><?= Html::a(Yii::t('app', 'News'), ['education/news']) ?></li>
                                             <li><?= Html::a(Yii::t('app', 'Me\'yoriy hujjatlar'), ['education/normative']) ?></li>
