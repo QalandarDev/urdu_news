0ff2b04e88bd390d2eb6c9eb19166434dfc01b87
DilshodDev <allashkurovdilshod@gmail.com>
2022-02-19 15:29:37 +0500

    asdasdf

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(__DIR__, 2) . '/frontend');
-Yii::setAlias('@uploads',   '/frontend/web');
+Yii::setAlias('@uploads',   '/frontend/web/');
 Yii::setAlias('@public', dirname(__DIR__, 2) . '/public');
 Yii::setAlias('@app', dirname(__DIR__, 2) . '/');
 Yii::setAlias('@backend', dirname(__DIR__, 2) . '/backend');
