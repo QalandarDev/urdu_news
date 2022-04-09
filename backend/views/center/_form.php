abcf04f84fafd6bbc167008dc7b1d72a21855802
DilshodDev <allashkurovdilshod@gmail.com>
2022-04-09 12:11:55 +0500

    input foto elfinder add

--- backend/views/center/_form.php
+++ backend/views/center/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use mihaildev\elfinder\InputFile;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -47,7 +48,15 @@ $cate= \yii\helpers\ArrayHelper::map($cateal,'id' ,'name_uz');
         ],
     ]);
     ?>
-    <?= $form->field($model, 'img')->fileInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'img')->widget(InputFile::class, [
+        'language' => 'ru',
+        'controller' => 'elfinder', // вставляем название контроллера, по умолчанию равен elfinder
+        'filter' => 'image',    // фильтр файлов, можно задать массив фильтров https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#wiki-onlyMimes
+        'template' => '<div class="input-group">{input}<span class="input-group-btn">{button}</span></div>',
+        'options' => ['class' => 'form-control'],
+        'buttonOptions' => ['class' => 'btn btn-default'],
+        'multiple' => false       // возможность выбора нескольких файлов
+    ]); ?>
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
