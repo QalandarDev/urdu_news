ad173b0778aaad57a54fdf568928513402eae1ba
UrSU_Admin <admin@urdu.uz>
2022-08-02 21:44:16 +0500

    +

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -56,7 +56,7 @@ $this->title = Yii::t('app', 'Urgench State University');
                                                                                 <a style="color: white;font-size: 18px" id="st"> <?= Yii::t('app','For applicants for the 2022-2023 academic year') ?></a><br><br>
                                             <div style="opacity:0.5; background-color:white;">
                                                 <div style="opacity:1.0;">
-                                                    <a style="color: red; font-weight: bold;  font-size: 18px" id="st"> <?= Yii::t('app','Diqqat!!! Ijodiy imtihon topshiruvchi abituriyentlarning imtihonda ishtirok etish vaqti e\'lon qilindi') ?></a><br><br>
+                                                    <a style="color: red; font-weight: bold;  font-size: 18px" id="st"> <?= Yii::t('app','Diqqat!!! Ijodiy imtihon topshiruvchi abituriyentlarning 1-avgust holatiga ko\'ra imtihonda natijalari e\'lon qilindi') ?></a><br><br>
 
                                                 </div>
                                             </div>
@@ -68,8 +68,8 @@ $this->title = Yii::t('app', 'Urgench State University');
                                                         <?= $more[$title] ?></a>
                                                 </li>
                                                 <li ><a style="color: red" id="st"
-                                                        href="https://urdu.uz/uz/site/active?id=198">
-                                                        <?= Yii::t('app','Ijodiylar uchun ') ?></a>
+                                                        href="https://urdu.uz/uz/site/active?id=199">
+                                                        <?= Yii::t('app','Kasbiy (Ijodiy) imtihon Natijalar') ?></a>
                                                 </li>
 
                                             </ul>
