932d0e22e0c615efcdb51df012f6ee5fbc6312b1
Qalandar Dev <64014854+QalandarDev@users.noreply.github.com>
2022-08-03 21:52:35 +0500

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -56,7 +56,7 @@ $this->title = Yii::t('app', 'Urgench State University');
                                                                                 <a style="color: white;font-size: 18px" id="st"> <?= Yii::t('app','For applicants for the 2022-2023 academic year') ?></a><br><br>
                                             <div style="opacity:0.5; background-color:white;">
                                                 <div style="opacity:1.0;">
-                                                    <a style="color: red; font-weight: bold;  font-size: 18px" id="st"> <?= Yii::t('app','Diqqat!!! Ijodiy imtihon topshiruvchi abituriyentlarning 2-avgust holatiga ko\'ra imtihonda natijalari e\'lon qilindi') ?></a><br><br>
+                                                    <a style="color: red; font-weight: bold;  font-size: 18px" id="st"> <?= Yii::t('app','Diqqat!!! Ijodiy imtihon topshiruvchi abituriyentlarning natijalari e\'lon qilindi') ?></a><br><br>
 
                                                 </div>
                                             </div>
